cdcdb2026e0b65eb9a020c69e6451e09d82d1774
Anushkajn228 <163176454+Anushkajn228@users.noreply.github.com>
2024-10-16 11:59:52 +0530

    Initial commit

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h6 className="footer-text">All rights reserved &copy; Anushka Jain</h6>
+    </div>
+  );
+};
+
+export default Footer;
